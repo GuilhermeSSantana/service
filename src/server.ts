94751a30e3c648eaf5772c5bdd0a89eab3b6b1a7
GuilhermeSantana <guissantana105@gmail.com>
2024-11-07 22:30:53 -0300

    config start project

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
+import dotenv from "dotenv";
 import express from "express";
 
-const app = express();
-const port = 3000;
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+const app = express();
+const PORT = process.env.PORT || 1818;
+
+app.use(express.json());
+// app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
